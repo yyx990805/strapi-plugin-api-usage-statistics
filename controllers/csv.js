bc8ddca2ad08e426e72b5bd455ae83508fe2e441
AZ <110185929+yyx990805@users.noreply.github.com>
2024-05-28 15:57:27 +0200

    csv

--- controllers/csv.js
+++ controllers/csv.js
@@ -1,7 +1,42 @@
 'use strict';
 
+// const { parseISO, startOfDay, endOfDay } = require('date-fns');
+
 module.exports = {
   async getCsv(ctx) {
+    try {
+      // const today = new Date();
+      // const start = startOfDay(today);
+      // const end = endOfDay(today);
+      // const stats = await strapi.query('api::stat.stat').findMany({
+      //   where: {
+      //     created_at: {
+      //       $gte: start,
+      //       $lte: end
+
+      const today = new Date();
+      const start = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+      const end = new Date(start);
+      end.setDate(end.getDate() + 1);
+      
+      const stats = await strapi.query('api::stat.stat').findMany({
+        where: {
+          createdAt: {
+            $gte: start.toISOString(),
+            $lt: end.toISOString()
+          }
+        }
+      });
+      const csvData = stats.map(row =>
+        Object.values(row).join(';')
+      ).join('\n');
+      ctx.set('Content-Type', 'text/plain');
+      ctx.send(csvData);
+    } catch (err) {
+      ctx.throw(500, err);
+    }
+  },
+  async getCsv2(ctx) {
     const csvData = 'a;b;c\n1;2;3';
     ctx.set('Content-Type', 'text/plain');
     // ctx.set('Content-Type', 'text/csv');
