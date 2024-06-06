fdf1ba06a45e02f401ad1cd6352fcf2e9ff88da5
AZ <110185929+yyx990805@users.noreply.github.com>
2024-06-06 14:37:56 +0200

    disable auto register (wrong final order of middleware exec)

--- strapi-server.js
+++ strapi-server.js
@@ -3,10 +3,6 @@ const controllers = require('./controllers/index');
 const routes = require('./routes/index');
 
 module.exports = () => ({
-  async bootstrap({ strapi }) {
-    strapi.server.use(middlewares.gatherStats);
-    // jsonInteractiveResponseMiddleware({ strapi });
-  },
   register(/** @typeof ({ strapi: import('@strapi/strapi').Strapi }) */{ strapi }) {
     if(0) strapi.server.routes([
       {
@@ -19,6 +15,10 @@ module.exports = () => ({
         },
       },
     ]);
+  },
+  async bootstrap({ strapi }) {
+    // strapi.server.use(middlewares.gatherStats); // FIXME, order is wrong (middleware is pushed to the list of middlewares (via plugins) too late)
+    // jsonInteractiveResponseMiddleware({ strapi });
   },
   middlewares,
   routes,
