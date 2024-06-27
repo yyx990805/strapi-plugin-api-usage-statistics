017ca72e4e030b3962767306c92729feba945316
AZ <110185929+yyx990805@users.noreply.github.com>
2024-06-27 15:12:52 +0200

    tests draft

---
+++ src/test.spec.js
@@ -0,0 +1,12 @@
+if(0)
+describe('', () => {
+  it('should mount get csv via registerMenu')
+  it('catch api calls')
+  it('create new api::stat entry if not found')
+  it('update existing api::stat entry if found')
+  it('extract resource id if filter[id][eq] provided')
+  it('generate csv file on specific route', () => {
+    fetch(`/strapi-plugin-api-usage-statistics/stats.csv`);
+  })
+  it.todo('catch visits to the pages')
+})
